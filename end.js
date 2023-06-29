b2fbb3e2418ae87b35ca18fc3459220c1412d830
Tolu Odunuga <to379@drexel.edu>
2023-06-29 09:25:41 -0400

    Initial commit

---
+++ end.js
@@ -0,0 +1,39 @@
+const username=document.querySelector("#username");
+const saveScoreBtn=document.querySelector("#saveScoreBtn");
+const finalScore=document.querySelector("#finalscore");
+const mostRecentScore=localStorage.getItem("mostRecentScore");
+
+const highScores=JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES=5;
+
+finalScore.innerText=mostRecentScore;
+
+username.addEventListener("keyup",()=>{
+    saveScoreBtn.disabled=!username.value;
+})
+
+saveHighScore=e=>{
+e.preventDefault();
+const score={
+    score:mostRecentScore,
+    name:username.value,
+
+}
+highScores.push(score);
+highScores.sort((a,b)=>{
+    return b.score-a.score;
+})
+highScores.splice(5);
+localStorage.setItem("highScores",JSON.stringify(highScores));
+window.location.assign("/");
+saveScoreBtn.disabled = true;
+
+
+
+
+
+}
+
+
+  
---
+++ highscores.js
@@ -0,0 +1,14 @@
+// const highScoresList=document.querySelector("#highScoresList");
+// const highScores=JSON.parse(localStorage.getItem("highScores")) || [];
+
+// highScoresList.innerHTML=
+// highScores.map(score=>{
+//     return <li class="high-score">${score.name}-${score.score}</li>
+// }).join("");
+
+const highScoresList = document.querySelector("#highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+highScoresList.innerHTML = highScores
+  .map((score) => `<li class="high-score">${score.name}-${score.score}</li>`)
+  .join("");
